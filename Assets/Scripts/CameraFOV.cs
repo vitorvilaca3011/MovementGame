e96da63f8362c9049ee8da6c7b91a0c414e408e5
Vitor Gabriel da Silva <vitorvilaca3011@gmail.com>
2025-04-24 15:08:17 -0300

    dynamic fov option
    24/04

--- Assets/Scripts/CameraFOV.cs
+++ Assets/Scripts/CameraFOV.cs
@@ -2,29 +2,27 @@ using UnityEngine;
 
 public class CameraFOV : MonoBehaviour
 {
-    //This is the field of view that the Camera has
-    public float m_FieldOfView;
-
-    void Start()
-    {
-        m_FieldOfView = 60.0f;
-    }
+    
+    public float baseFOV = 60f;
+    public float maxFOV = 90f;
+    public float fovSpeedFactor = 0.2f; // speed that fov changes
+    public bool dynamicFov = true; // if true, fov changes based on speed
+    [SerializeField]private Rigidbody playerRb;
+    [SerializeField]private Camera cam;
 
     void Update()
     {
-        //Update the camera's field of view to be the variable returning from the Slider
-        Camera.main.fieldOfView = m_FieldOfView;
-    }
+        if(dynamicFov == true)
+        {
+        float speed = playerRb.velocity.magnitude;
+        float targetFOV = baseFOV + (speed * fovSpeedFactor);
+        targetFOV = Mathf.Clamp(targetFOV, baseFOV, maxFOV);
 
-    void OnGUI()
-    {
-        //Set up the maximum and minimum values the Slider can return (you can change these)
-        float max, min;
-        max = 150.0f;
-        min = 20.0f;
-        //This Slider changes the field of view of the Camera between the minimum and maximum values
-        m_FieldOfView = GUI.HorizontalSlider(new Rect(20, 20, 100, 40), m_FieldOfView, min, max);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, Time.deltaTime * 5f);
+        }
+        else
+        {
+            cam.fieldOfView = baseFOV;
+        }
     }
-
-    
 }
